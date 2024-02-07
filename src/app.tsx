e90851dd6cc24d721e46609bc7098a214085c530
mrc-augusto <mrc_augusto@hotmail.com>
2024-02-07 18:09:57 -0300

    commit day 2

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+const note={
+  date:new Date(),
+  content: 'Hello World'
+}
+
 export function App() {
   return(
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -21,9 +26,8 @@ export function App() {
 
         <NewNoteCard/>
         
-        <NoteCard/>
-        <NoteCard/>
-        <NoteCard/>
+        <NoteCard note={note}/>
+        
 
       </div>
         
